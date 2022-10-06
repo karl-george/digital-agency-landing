5bbc425df02a684b8f3b649aa13324b159a87bee
Karl George <karlgdev@gmail.com>
2022-10-06 14:03:50 +0200

    Working on header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import headerImg from '../../assets/header-img.png';
+import clockImg from '../../assets/clock.png';
+import { BsArrowUpRight } from 'react-icons/bs';
 import './header.css';
 
 const Header = () => {
@@ -6,17 +8,24 @@ const Header = () => {
     <div className='header section__padding'>
       <div className='header__container'>
         <div className='header__container-content'>
-          <p>Jump start your growth</p>
+          <p className='content__highlight'>Jump start your growth</p>
           <h1>
             We boost the growth for <span>Startup</span> to Fortune 500
-            Companies
+            Companies{' '}
+            <span className='content__icon'>
+              <img src={clockImg} />
+            </span>
           </h1>
-          <p>
-            Get the most accurate leads, sales peopele training and conversions,
+          <p className='content_blurb'>
+            Get the most accurate leads, sales people training and conversions,
             tools and more — all within the same one billing.
           </p>
-          <input></input>
-          <button type='button'></button>
+          <div className='header__container-content-input'>
+            <input type='email' placeholder='Email address'></input>
+            <button type='button'>
+              <BsArrowUpRight color='#111124' size={25} />
+            </button>
+          </div>
         </div>
         <div className='header__container-image'>
           <img src={headerImg} alt='header' />
