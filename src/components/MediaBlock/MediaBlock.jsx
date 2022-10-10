f191510e2e5a4bf0818f9e8a872ac2bfa926412d
Karl George <karlgdev@gmail.com>
2022-10-10 14:32:00 +0200

    Finishing up media section

--- src/components/MediaBlock/MediaBlock.jsx
+++ src/components/MediaBlock/MediaBlock.jsx
@@ -4,14 +4,12 @@ const MediaBlock = ({ img, title, content, desc, link }) => {
   return (
     <div className='mediablock'>
       <img src={img} alt={`${img}`} />
-      <div className='mediablock_content'>
+      <div className='mediablock-content'>
         <h4>{desc}</h4>
         <h3>{title}</h3>
         <p>{content}</p>
-        <a>
-          <p>
-            Learn more about {link} {`->`}
-          </p>
+        <a href='#'>
+          <p>Learn more about {link} &rarr;</p>
         </a>
       </div>
     </div>
